7b9e8846b426ce2e38db7c6d8f940b8eaa1722c9
Jeff <jgomez630@aol.com>
2018-05-24 15:22:12 -0700

    sec-4_ch-41_updating_scores_change_active_player

--- app.js
+++ app.js
@@ -22,9 +22,32 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+	// 1. Random number
 	var dice = Math.floor(Math.random() * 6) + 1;
+	
+	// 2. Display result
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';	
 	diceDOM.src = 'dice-' + dice + '.png';
+
+	//3. Update roundScore IF the rolled number is not a 1. 
+	if (dice !== 1){
+		// Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;		
+	} else {
+		// Next player (ternary)
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = 0;
+		document.getElementById('current-1').textContent = 0;
+		// Toggles active class from current player 
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		// Resets dice display for next player
+		diceDOM.style.display = 'none';
+	}
 });
 	
