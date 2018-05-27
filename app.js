9ab9b971f38ed395c0b437ffccb95bb818976eb2
Jeff <jgomez630@aol.com>
2018-05-26 22:01:37 -0700

    sec-4_ch-42_hold_function

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
+//Global Variables 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+scores = [0,0]; //stores scores for both players
+roundScore = 0; //stores scores for current round
+activePlayer = 0; //stores scores for active player
 
+// Set dice image to hidden
 document.querySelector('.dice').style.display = 'none';
 
+// Set roundScore & activePlayer score to zero
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
+// Roll dice button logic using an eventListener
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	// 1. Random number
 	var dice = Math.floor(Math.random() * 6) + 1;
@@ -36,6 +39,29 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		roundScore += dice;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;		
 	} else {
+		nextPlayer();
+	}
+});
+	
+	document.querySelector('.btn-hold').addEventListener('click', function(){
+	// add current score to global score
+		scores[activePlayer] += roundScore;
+
+	// Update UI 
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// Check if player won the game
+	if (scores[activePlayer] >= 10){
+	document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+	document.querySelector('.dice').style.display = 'none';
+	document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); 
+	} else {
+		nextPlayer();
+	}
+});
+
+	function nextPlayer() {
 		// Next player (ternary)
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 		roundScore = 0;
@@ -48,6 +74,5 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 		// Resets dice display for next player
 		diceDOM.style.display = 'none';
-	}
-});
+	}	
 	
